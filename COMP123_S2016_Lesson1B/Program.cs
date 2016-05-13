fe4d157bc6f9fb7abb39892f735af88d0813101b
sbalac17 <sbalac17@my.centennialcollege.ca>
2016-05-13 11:59:51 -0400

    Added a test

---
+++ COMP123_S2016_Lesson1B/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Tom Tsiliopoulos
+ * Date Created: May 13, 2016
+ * Date Modified: May 13, 2016
+ * Student ID: 30018557
+ * 
+ * Description: This program demonstrates GitHub and version control
+ * best practices. We also explore Unit Testing
+ * 
+ * Version: 0.4 - extracted OutputStringToConsole from GetUserName method
+ */
+
+
+namespace COMP123_S2016_Lesson1B
+{
+    /**
+     * This is the driver class for our program
+     * 
+     * @class Program
+     */
+    class Program
+    {
+        /**
+         * Main Method for class Program
+         * 
+         * @param {string[]} args
+         * @constructor Main
+         */
+        static void Main(string[] args)
+        {
+            GetUserName();
+        }
+
+
+        /**
+         * This method reads the UserName from the Console
+         * 
+         * @method GetUserName
+         * @return {string} UserName
+         */
+        public static string GetUserName()
+        {
+            // Initialize Variables
+            string UserName = "";
+
+            OutputStringToConsole("Enter UserName: ", false);
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("\n=========================\n");
+            Console.WriteLine("You Entered: " + UserName + "\n");
+
+            return UserName;
+        }
+
+        /**
+         * This method writes a string to the console. Parameters allow the user to
+         * choose to add a newLine character
+         * 
+         * @method OutputStringToConsole
+         * @param {string} outputString
+         * @param {bool} hasNewLine
+         * @return {string} outputString
+         */
+        private static string OutputStringToConsole(string outputString, bool hasNewLine)
+        {
+            if (hasNewLine)
+            {
+                Console.WriteLine(outputString);
+            }
+            else
+            {
+                Console.Write(outputString);
+            }
+
+            return outputString;
+        }
+    }
+}
---
+++ COMP123_S2016_Lesson1B/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("COMP123_S2016_Lesson1B")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("COMP123_S2016_Lesson1B")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be2d4adb-985e-4cb9-9493-6eca9824553f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
